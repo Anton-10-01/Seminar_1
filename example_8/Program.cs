effb87580f34b24ed3f0538fa065069fb1ada05e
Anton <razjivin.anton@yandex.ru>
2023-06-26 22:18:41 +0300

    add example_8

---
+++ example_8/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задание №8 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Write("Введи число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+int count = 1;
+while (count <= number) {
+    if (count % 2 == 0) Console.Write(count);
+    if (count % 2 != 0 && count != 1 && count < number) Console.Write(", ");
+    count++;
+}
