96310ef8b89a76b12655fa9d3b00cae9f82d205a
Anton <razjivin.anton@yandex.ru>
2023-06-26 22:08:46 +0300

    add example_6

---
+++ example_6/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Здача №6 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.Write("Введи число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+if (number % 2 == 0) Console.WriteLine("Да");
+else {
+    Console.WriteLine("Нет");
+}
